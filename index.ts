f030bd1bf1ade4d33db241f4871597ca3afdc3df
muhammadnazimuddinkhan <165476534+muhammadnazimuddinkhan@users.noreply.github.com>
2024-04-16 09:18:17 +0500

    create number guessing

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+    {
+       message: "please guess a number",
+       type: "number" ,
+       name:"userguessednumber",
+    }
+])
+if(answer.userguessednumber === randomnumber){
+    console.log("congratulation ! you win the game")
+}else {
+    console.log("please try again");
+
+}
